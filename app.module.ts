c906e6e0b3a89c8fa96bfee36e684f3bccf04100
reza.duty <reza.duty.persian@gmail.com>
2025-07-09 00:03:11 +0200

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { WindowRef } from './../WindowRef';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [WindowRef],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
